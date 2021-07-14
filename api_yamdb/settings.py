c6a36be64a74ba7766d081dba856dccd4b64bd4b
Сергей <7atom7@mail.ru>
2021-07-14 12:53:37 +0300

    Sp_18_First

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -89,11 +89,11 @@ DATABASES = {
        # 'ENGINE': 'django.db.backends.sqlite3',
        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
          'ENGINE': 'django.db.backends.postgresql',
-         'NAME': os.environ.get('DB_NAME'),
-         'USER': os.environ.get('POSTGRES_USER'),
-         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-         'HOST': os.environ.get('DB_HOST'),
-         'PORT': os.environ.get('DB_PORT'),
+         'NAME': os.environ['DB_NAME'],
+         'USER': os.environ['POSTGRES_USER'],
+         'PASSWORD': os.environ['POSTGRES_PASSWORD'],
+         'HOST': os.environ['DB_HOST'],
+         'PORT': os.environ['DB_PORT'],
     }
 }
 
